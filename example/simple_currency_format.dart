ae5e5ab449720b2099eaba62f6357a6ba6e282cc
noninus <rafinha.nonino7@gmail.com>
2021-01-18 11:52:12 -0300

    added comments on example filde

--- example/simple_currency_format.dart
+++ example/simple_currency_format.dart
@@ -1,9 +1,9 @@
 import 'package:simple_currency_format/simple_currency_format.dart';
 
 main() {
-  print(currencyFormat(10));
-  print(currencyFormat(12.3));
-  print(currencyFormat(5, locale: 'en_US', symbol: '\$ '));
+  print(currencyFormat(10)); //returns R$ 10,00
+  print(currencyFormat(12.3)); //returns R$ 13,30
+  print(currencyFormat(5, locale: 'en_US', symbol: '\$ ')); //returns $ 5.00
 
-  print(removeZeroDecimal(00500));
+  print(removeZeroDecimal(00500)); //returns 500
 }
